4680ecf94bda36c8a586f2d5f9b51afbdd2bc552
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-03 18:20:03 +0700

    list of objects

--- src/components/List.js
+++ src/components/List.js
@@ -7,16 +7,34 @@ export default class List extends Component {
     constructor(props) {
         super(props);
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-        this.state = { name: ds.cloneWithRows(['Pho', 'Khoa', 'Huong']) };
+        this.state = { notes: ds.cloneWithRows(arrNote) };
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#CCE1F6' }}>
                 <ListView 
-                    dataSource={this.state.name}
-                    renderRow={name => <Text>{name}</Text>}
+                    dataSource={this.state.notes}
+                    renderRow={notes => (
+                        <View>
+                            <Text style={{ color: '#952424' }}>{notes.subject}</Text>
+                            <Text style={{ color: '#80A4DA' }}>{notes.content}</Text>
+                        </View>
+                    )}
                 />
             </View>
         );
     }
 }
+
+class NoteModel {
+    constructor(subject, content) {
+        this.subject = subject;
+        this.content = content;
+    }
+}
+
+const arrNote = [
+    new NoteModel('Hoc Tap', 'Lam do an React Native'),
+    new NoteModel('Cong viec', 'Lam bao cao cuoi thang'),
+    new NoteModel('Vui choi', 'Di du lich le 1/5')
+];
